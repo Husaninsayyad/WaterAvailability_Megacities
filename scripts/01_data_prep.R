cd399691eced27a8b3abd380bc922d45baf6e522
Hussainsayyad <alihussainsyed026@gmail.com>
2026-04-07 07:05:46 +0200

    Data Preparation script

--- scripts/01_data_prep.R
+++ scripts/01_data_prep.R
@@ -1,27 +1,29 @@
-getwd()
-my_wd <- getwd()
-setwd(my_wd)
-setwd('C:/Users/Admin/OneDrive - CZU v Praze/Plocha/Thesis/Paper1/MSWEP')
+# 01_data_prep.R
+
 library(dplyr)
 library(readxl)
-# reading the data sent by Vishal 
-data <- readRDS('C:/Users/Admin/OneDrive - CZU v Praze/Plocha/Thesis/Paper1/p_e_pet_cities_data_final1.rds')
-continent_data <- read_excel("continent_data_clean.xlsx")
+
+data <- readRDS("data/p_e_pet_cities_data_final1.rds")
+continent_data <- read_excel("data/continent_data_clean.xlsx")
+
+# extract year from date and drop the date column
+data <- data %>%
+  mutate(year = format(as.Date(date), "%Y")) %>%
+  select(-date)
 
 data <- data %>%
-  mutate(year = format(as.Date(date), "%Y")) %>%  
-  select(-date)  
-data <- data %>%
-  left_join(continent_data %>% select(city , Continent), by = "city")
+  left_join(continent_data %>% select(city, Continent), by = "city")
+
+# mswx-past is excluded from the analysis
 data <- data %>%
   filter(data_source != "mswx-past")
 
-#changing the name of mswep and gleam into one common name 
-unique(data1$data_source)
-data1 %>% distinct(data_source, variable)
+# gleam and mswep are treated as one combined source
 data1 <- data %>%
   mutate(data_source = recode(data_source,
                               "gleam-v4-1a" = "mswep/gleam",
-                              "mswep-v2-8" = "mswep/gleam"))
+                              "mswep-v2-8"  = "mswep/gleam"))
 
+unique(data1$data_source)
+data1 %>% distinct(data_source, variable)
 
